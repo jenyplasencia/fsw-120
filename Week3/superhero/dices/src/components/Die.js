58eb54d6b6aedcc311accff36fdc1f5a1b84ddf3
jenyplasencia <70494892+jenyplasencia@users.noreply.github.com>
2021-02-03 12:23:34 -0500

    Add files via upload

---
+++ Week3/superhero/dices/src/components/Die.js
@@ -0,0 +1,12 @@
+import React from "react"
+/* export let box = document.getElementById("box") */
+
+function Die(props) {
+  return (
+    <div>
+      <h3>{props.num}</h3>
+    </div>
+  )
+}
+
+export default Die;
